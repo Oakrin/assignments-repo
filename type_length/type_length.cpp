4b4af18d474a5ce8e7853dda0dd3e79490c9270a
Oakrin <orinking@gmail.com>
2024-09-15 23:31:28 -0700

    type_length program done

---
+++ type_length/type_length.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+
+int main(){
+    short shawty = (short)123;
+    double dubious = 321;
+    
+    std::cout << "the size of short is " << sizeof(shawty) << std::endl;
+    std::cout << "the size of double is " << sizeof(dubious) << std::endl;
+
+    return 0;
+}
