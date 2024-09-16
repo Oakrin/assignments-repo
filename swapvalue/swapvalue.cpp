6138c1db44c68e6a66b47e584ac84dedba4ef861
Oakrin <orinking@gmail.com>
2024-09-15 23:17:22 -0700

    d6 assignment completed

--- swapvalue/swapvalue.cpp
+++ swapvalue/swapvalue.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 
 //this program swaps two values
+
+void swap(int& num1, int& num2)
+{
+    int temp = num1;
+    num1 = num2;
+    num2 = temp;
+};
+
 int main()
 {
     int a = 1;
     int b = 2;
 
-    int temp = a;
-    a = b;
-    b = temp;
+    swap(a, b);
 
-    std::cout << "a = " << a << "\nb = " << b << std::endl;
+    std::cout << "a = " << a << "\nb = " << b;
     return 0;
 }
